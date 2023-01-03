a25a671f91664dbdd6eea150bd16b096af047f72
summerstar-555 <3577295397@qq.com>
2023-01-03 16:53:36 +0800

    2023年1月3日16:53:32

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 import requests
 from lxml import etree
 import time
+from search_result import search_result
 
 
 def print_tags():  # 输出标签
@@ -29,9 +30,12 @@ def image_link_process(image_link: str) -> str:
 
 
 print_tags()  # 输出标签
+tag = input("\n请输入想搜索的内容标签：")
+# 从这里开始显示查找标签的结果
+search_result(tag)
 while 1:
-    tag = input("\n请输入想搜索的内容标签：")
-    url = f'https://wallhaven.cc/search?q={tag}'
+    page_num = 1
+    url = f'https://wallhaven.cc/search?q={tag}&page{page_num}'
     small_xpath = '//img[@alt="loading"]/@data-src'  # 小图片的xpath路径
     i = 0
     j = 0
@@ -39,6 +43,8 @@ while 1:
     e = etree.HTML(r.text)
     small_list = e.xpath(small_xpath)  # 获取一页小图片的链接
     pic_path = f'./Pictures/{tag}/'  # 根据用户输入的标签进行创建文件夹
+    if not os.path.exists('./Pictures'):
+        os.mkdir('./Pictures')
     if not os.path.exists(pic_path):  # 检查是否存在
         os.mkdir(pic_path)
     for link in small_list:
@@ -53,24 +59,7 @@ while 1:
             with open(f'./Pictures/{tag}/%s.jpg' % time.time(), 'wb') as f:  # 使用时间戳命名
                 f.write(fullpic_resp.content)
     print(f"一共下载了{j}张图片，下载失败的图片一共有{i}张")
+    page_num = page_num + 1
     answer = input("是否退出:")
     if answer == '是':
         break
-
-
-# 这是一个示例 Python 脚本。
-
-# 按 Shift+F10 执行或将其替换为您的代码。
-# 按 双击 Shift 在所有地方搜索类、文件、工具窗口、操作和设置。
-
-
-def print_hi(name):
-    # 在下面的代码行中使用断点来调试脚本。
-    print(f'Hi, {name}')  # 按 Ctrl+F8 切换断点。
-
-
-# 按间距中的绿色按钮以运行脚本。
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
